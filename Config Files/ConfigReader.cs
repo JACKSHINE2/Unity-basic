5bd7b7f907f50e0e9e95dd1746ba5798329ea40d
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-02-02 16:28:59 +0800

    register

---
+++ Config Files/ConfigReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+
+public class ConfigReader1 : MonoBehaviour {
+    string path = "Invoke.txt";
+	// Use this for initialization
+	void Start () {
+        //1.文件真实路径
+        path = Path.Combine(Application.streamingAssetsPath,path);
+        //2.读取所有内容
+        print(File.ReadAllText(path));
+        //3.每一行进行读取
+        //string[] str = File.ReadAllLines(path);
+        //for (int i = 0; i < str.Length; i++)
+        //{
+        //    print(str[i]);
+        //}
+        ////4.字节读取，移动端
+        //byte[] buffer = File.ReadAllBytes(path);
+        //string item = Encoding.UTF8.GetString(buffer);
+        //print(item);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Config Files/InforShow.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
+using System.IO;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public static class Hero
+{
+    public static int Hp;
+    public static int Mp;
+    public static int Att;
+    public static int Def;
+    public static int Coin;
+    public static int Inte;
+}
+   
+
+
+public class InforShow : EventTrigger {
+    string path = "Goods.txt";
+    Transform inforShow;
+    string[] info;
+    Text message;
+    int liner;
+    string[] str;
+    int value;
+    // Use this for initialization
+    void Start () {
+        inforShow = GameObject.FindWithTag("Finish").transform.GetChild(1);
+        message = inforShow.GetChild(0).GetComponent<Text>();
+        inforShow.gameObject.SetActive(false);
+        path = Path.Combine(Application.streamingAssetsPath,path);
+        info = File.ReadAllLines(path);
+        liner = gameObject.name[5] - 48;
+        str = info[liner].Split(',');
+        value = int.Parse(str[2]);
+    }
+    public override void OnPointerEnter(PointerEventData eventData)
+    {
+        inforShow.gameObject.SetActive(true);
+        inforShow.position = Input.mousePosition;
+        message.text = info[liner];
+    }
+
+    public override void OnPointerExit(PointerEventData eventData)
+    {
+        inforShow.gameObject.SetActive(false);
+    }
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        switch(liner)
+        {
+            case 1:
+                {
+                    Hero.Hp += value;
+                    break;
+                }
+            case 2:
+                {
+                    Hero.Mp += value;
+                    break;
+                }
+            case 3:
+                {
+                    Hero.Def += value;
+                    break;
+                }
+            case 4:
+                {
+                    Hero.Coin += value;
+                    break;
+                }
+            case 5:
+                {
+                    Hero.Inte += value;
+                    break;
+                }
+            case 6:
+                {
+                    Hero.Att += value;
+                    break;
+                }
+        }
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        inforShow.position = Input.mousePosition;
+
+    }
+}
