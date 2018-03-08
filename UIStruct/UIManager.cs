b2301f47f16ca6951d91a4856261c49887fb3b6d
Jack <33677306+JACKSHINE2@users.noreply.github.com>
2018-03-08 17:08:21 +0800

    UIStruct

---
+++ UIStruct/UIManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 所有UIPanel的管理  
+/// </summary>
+public class UIManager : MonoBehaviour {
+
+    //获取到所有的UIPanle  UIScene
+    Dictionary<string, UISence> uiScenes = new Dictionary<string, UISence>();
+
+    /// <summary>
+    /// 初始化
+    /// </summary>
+    public void Init()
+    {
+        object[] uiScene = GameObject.
+            FindObjectsOfType(typeof(UISence));
+        UISence item=null;
+        //tem = this.gameObject;
+       // print(item.name);
+        for (int i = 0; i < uiScene.Length; i++)
+        {
+            print(uiScene[i]);
+            item = (UISence)uiScene[i];
+            print(item.name);
+            uiScenes.Add(item.name, item);
+            item.gameObject.SetActive(false);
+        }
+    }
+    /// <summary>
+    /// 显示一级界面
+    /// </summary>
+    public void ShowUI()
+    {
+        //打开   关闭
+        OpenUiScene(UISenceName.Panel_Main, true);
+    }
+    /// <summary>
+    /// 打开或者关闭界面
+    /// </summary>
+    /// <param name="name">要打开/关闭的界面名称</param>
+    /// <param name="isSceneOpen">打开或者关闭</param>
+    public void OpenUiScene(string name,bool isSceneOpen)
+    {
+        if(uiScenes.ContainsKey(name))
+        {
+            uiScenes[name].
+                gameObject.SetActive(isSceneOpen);
+
+        }
+    }
+}
+public class UISenceName
+{
+    public const string Panel_Main = "Panel_Main";
+    public const string Panel_OpenScene = "Panel_OpenScene";
+
+}
---
+++ UIStruct/UISence.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISence : MonoBehaviour {
+
+    //递归   查找子物体的
+    public void Start()
+    {
+        //   Transform t=  SceneHelper.FindChild(this.transform, "Label");
+        //  print(t.name);
+        print(SceneHelper.FindChild
+            <Text>(this.transform, "Label").text);
+    }
+}
+public class SceneHelper
+{
+    public static Transform FindChild(Transform item,string name)
+    {
+        Transform t = null;
+        t = item.Find(name);
+        if (t != null) return t;
+        for (int i = 0; i < item.childCount; i++)
+        {
+            t = FindChild(item.GetChild(i),name);
+            if (t != null)
+                return t;
+        }
+        return null;
+    }
+    public static T FindChild<T>(Transform item, string name)
+        where T : Component
+    {
+        Transform t = null;
+        t = item.Find(name);
+        if (t != null) return t.GetComponent<T>();
+        for (int i = 0; i < item.childCount; i++)
+        {
+            t = FindChild(item.GetChild(i), name);
+            if (t != null)
+                return t.GetComponent<T>();
+        }
+        return null;
+    }
+
+}
